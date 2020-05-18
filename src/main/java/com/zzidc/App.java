acf8b29280a965037755c5a8dd5de8bd3eeab724
luokeke <1326677806@qq.com>
2020-05-18 10:38:18 +0800

    1

---
+++ src/main/java/com/zzidc/App.java
@@ -0,0 +1,13 @@
+package com.zzidc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
